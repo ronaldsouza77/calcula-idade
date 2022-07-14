6504c2201f6410e9c90931f07259d5459de25d53
Ronald <ronalddesouzagaldino@gmail.com>
2022-07-14 10:04:46 -0300

    Correção de sintaxe

--- src/App.java
+++ src/App.java
@@ -5,7 +5,7 @@ public class App {
     public static void main(String[] args) {
         Scanner entrada = new Scanner(System.in);
         System.out.print("Em que ano você nasceu? ");
-        int ano = Integer.parseInt(entrada.nextLine())
+        int ano = Integer.parseInt(entrada.nextLine());
         int anoAtual = Calendar.getInstance().get(Calendar.YEAR);
         int idadeEsseAno = anoAtual - ano;;
         System.out.print("Este ano você faz " + idadeEsseAno + " anos.");
